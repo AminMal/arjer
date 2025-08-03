2abe95e1b2674434c9777035fc5545d5c598154b
Amin Malekloo <amin.malekloo@persgroep.net>
2025-08-03 15:20:43 +0200

    move parse function to module arjer

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 pub mod json;
 mod core;
 
-pub mod parser {
-    use crate::core::{parse_tokens, tokenize};
-    use crate::json::JsValue;
+use crate::core::{parse_tokens, tokenize};
+use crate::json::JsValue;
 
-    pub fn parse<T: Into<String>>(t: T) -> Result<JsValue, String> {
-        parse_tokens(tokenize(t.into())?)
-    }
+pub fn parse<T: Into<String>>(t: T) -> Result<JsValue, String> {
+    parse_tokens(tokenize(t.into())?)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::fs;
 use std::time::Instant;
 
-use arjer::parser;
 
 fn main() {
     let start = Instant::now();
     let input = fs::read_to_string("input.json").unwrap();
-    let json = parser::parse(input).unwrap();
+    let json = arjer::parse(input).unwrap();
     let end = Instant::now();
     let dur = end.duration_since(start);
     dbg!(json);
